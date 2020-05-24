c60ab79318e0b3ca0a9ae33b1828f3c578212f55
김현석 <devhyunseok@gmail.com>
2020-05-24 17:06:39 +0900

    Initialize webpack config

---
+++ config/webpack.dev.config.js
@@ -0,0 +1,34 @@
+const paths = require('./paths');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: "development",
+  entry: `${paths.appSrc}/index.tsx`,
+  output: {
+    path: paths.dist,
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+  devtool: "cheap-module-source-map",
+  devServer: {
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_module/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: paths.appHtml
+    })
+  ]
+};
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App:React.FC = () => {
+  return (
+    <div>
+      <h4>React App</h4>
+    </div>
+  )
+};
+
+export default App;
